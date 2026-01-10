0a2d4be8ee683030254b29472ed1f5b5c4d71c6b
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2026-01-10 10:34:22 -0800

    Update index.js
    testing for loop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
@@ -6,3 +6,8 @@ inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+// Log out the items in the myLeads array using a for loop
+for (let i=0; i< myLeads.length; i++) {
+    console.log(myLeads[i])
+}
